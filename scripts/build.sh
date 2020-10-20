0919a969e50b4ca5c1a720e3c76aa5bffb7d5f3b
Nathan Dench <ndenc2@gmail.com>
2020-10-20 21:09:47 +1000

    fix: Build index.d.ts without modules

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
 DIST_DIRECTORY="dist"
-TEMP_DIRECTORY="temp-dec"
 
 # Remove current dist and temp directory
 rm -rf $DIST_DIRECTORY
-rm -rf $TEMP_DIRECTORY
 
 # Build the whole library
 rollup -c
 
-# Move TS Declarations out to a temporary folder
-for i in $(find "$DIST_DIRECTORY" -type f -name "*.d.ts"); do
-    DIRECTORY=$(dirname "${i}")
-    if [ "$DIRECTORY" == "dist" ]
-    then
-        DIRECTORY=""
-    else
-        DIRECTORY=${DIRECTORY#"$DIST_DIRECTORY/"}
-    fi
-    mkdir -p "$TEMP_DIRECTORY/$DIRECTORY"
-
-    mv $i "$TEMP_DIRECTORY/$DIRECTORY"
-done
-
 # Generate single TS Declaration file
 dts-bundle --configJson ./dts-bundle.json
 
-# Remove temp folders
-rm -rf $TEMP_DIRECTORY
+# dts-bundle leave a bunch of empty directories around
 find $DIST_DIRECTORY -type d -empty -delete
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 DIST_DIRECTORY="dist"
-TEMP_DIRECTORY="temp-dec"
 
-# Remove current dist and temp directory
+# Remove current dist
 rm -rf $DIST_DIRECTORY
-rm -rf $TEMP_DIRECTORY
 
 # Build the whole library
 rollup -cw
